b4556bf7b29d50b7d15de6d3883c4832482d6b9c
craigjullie <craigjullie@gmail.com>
2019-06-14 13:48:55 -0500

    added user agent parser app

---
+++ user_agent_parsing/CreateJSONFile/MainMod.vb
@@ -0,0 +1,107 @@
+﻿Imports System.Configuration.ConfigurationManager
+Imports System.Data.SqlClient
+Imports System.IO
+Imports System.Net
+Imports System.Net.Security
+Imports System.Security.Cryptography.X509Certificates
+
+Module MainMod
+
+  Dim _ConnString As String = ConnectionStrings("TestDB").ConnectionString
+
+  Sub Main()
+    Dim UserAgentData As DataTable = GetUserAgentData()
+    Dim JSONList As List(Of String) = BuildJSONList(UserAgentData)
+    Dim PostData As String = BuildJSONPost(JSONList)
+    SubmitJSONRequest(PostData)
+    Console.ReadKey()
+  End Sub
+
+  Private Function GetUserAgentData() As DataTable
+    Dim Conn As New SqlConnection(_ConnString)
+    Dim Cmd As New SqlCommand("select * from UserAgentData order by ID ASC")
+    Dim Adapter As New SqlDataAdapter
+    Dim UserAgentData As New DataTable
+    Conn.Open()
+    Cmd.Connection = Conn
+    Adapter.SelectCommand = Cmd
+    Adapter.Fill(UserAgentData)
+    Conn.Close()
+    Return UserAgentData
+  End Function
+
+  Private Function BuildJSONList(ByVal userAgentData As DataTable) As List(Of String)
+    Dim JSONList As New List(Of String)
+    Dim JSONLine As String
+    For Each Row As DataRow In userAgentData.Rows
+      JSONLine = "  """ & Row("ID").ToString & """: """ & Row("UserAgent").ToString & """"
+      JSONList.Add(JSONLine)
+    Next
+    Return JSONList
+  End Function
+
+  Private Function BuildJSONPost(ByVal jsonList As List(Of String)) As String
+    Dim PostData As String = "{""user_agents"": {"
+    For Each Line As String In jsonList
+      PostData &= vbCrLf & Line
+    Next
+    PostData &= vbCrLf & "}}"
+    Return PostData
+  End Function
+
+  Private Sub SubmitJSONRequest(ByVal postData As String)
+    Dim Request As HttpWebRequest
+    Dim Response As HttpWebResponse
+    Dim Reader As StreamReader
+    Dim RawResponse As String
+
+    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12
+    'https://api.whatismybrowser.com/api/v2/user_agent_parse_batch
+    Request = DirectCast(WebRequest.Create("https://api.whatismybrowser.com/api/v2/user_agent_parse_batch"), HttpWebRequest)
+    Request.Headers.Add("X-API-KEY", "91b8b3483b699ccadd3cfff06a742d03")
+    Request.ContentType = "application/json"
+    Request.Method = "POST"
+    Request.ContentLength = postData.Length
+
+    Dim RequestWriter As New StreamWriter(Request.GetRequestStream())
+    RequestWriter.Write(postData)
+    RequestWriter.Close()
+
+    Response = DirectCast(Request.GetResponse(), HttpWebResponse)
+    Reader = New StreamReader(Response.GetResponseStream())
+    RawResponse = Reader.ReadToEnd()
+    Console.ReadKey()
+  End Sub
+
+  Private Sub SubmitJSONRequestV2(ByVal postData As String)
+    ServicePointManager.ServerCertificateValidationCallback = New Security.RemoteCertificateValidationCallback(AddressOf ValidateRemoteCertificate)
+    Dim Resp As HttpWebResponse
+    Dim DataStream As Stream
+    Dim Request As HttpWebRequest = HttpWebRequest.Create("https://api.whatismybrowser.com/api/v2/user_agent_parse_batch")
+
+    Request.Headers.Add("X-API-KEY", "91b8b3483b699ccadd3cfff06a742d03")
+    Request.Accept = "*/*"
+    Request.Method = "POST"
+    Dim ByteArray As Byte() = System.Text.Encoding.[Default].GetBytes(postData)
+    Request.ContentType = "application/json"
+    Request.ContentLength = ByteArray.Length
+    DataStream = Request.GetRequestStream()
+    DataStream.Write(ByteArray, 0, ByteArray.Length)
+    DataStream.Close()
+
+    Resp = Request.GetResponse()
+    Dim DataStreamReturn As Stream = Resp.GetResponseStream()
+    Dim Reader As New StreamReader(DataStreamReturn)
+    Dim ResponseFromServer As String = Reader.ReadToEnd()
+    Console.ReadKey()
+  End Sub
+
+  Private Function ValidateRemoteCertificate(ByVal sender As Object, ByVal certificate As X509Certificate, ByVal chain As X509Chain, ByVal policyErrors As SslPolicyErrors) As Boolean
+    If policyErrors = System.Net.Security.SslPolicyErrors.None Then
+      Return True
+    End If
+    Console.WriteLine("X509Certificate [{0}] Policy Error: '{1}'", certificate.Subject, policyErrors.ToString)
+    Return False
+  End Function
+
+End Module
---
+++ user_agent_parsing/UserAgentTesting/MainMod.vb
@@ -0,0 +1,105 @@
+﻿Imports System.Configuration.ConfigurationManager
+Imports System.Data.SqlClient
+Imports UAParser
+
+Module MainMod
+
+  Dim _ConnString As String = ConnectionStrings("TestDB").ConnectionString
+
+  Sub Main()
+    Dim UserAgentData As DataTable = GetUserAgentData()
+    For Each Row As DataRow In UserAgentData.Rows
+      UpdateRecord(Row)
+    Next
+    Using Conn As New SqlConnection(_ConnString)
+      Conn.Open()
+      Using BulkCopy As New SqlBulkCopy(Conn)
+        BulkCopy.DestinationTableName = "UserAgentDataRerun"
+        BulkCopy.WriteToServer(UserAgentData)
+      End Using
+      Conn.Close()
+    End Using
+  End Sub
+
+  Private Function GetUserAgentData() As DataTable
+    Dim Conn As New SqlConnection(_ConnString)
+    Dim Cmd As New SqlCommand("select * from UserAgentData order by ID ASC")
+    Dim Adapter As New SqlDataAdapter
+    Dim UserAgentData As New DataTable
+    Conn.Open()
+    Cmd.Connection = Conn
+    Adapter.SelectCommand = Cmd
+    Adapter.Fill(UserAgentData)
+    Conn.Close()
+    Return UserAgentData
+  End Function
+
+  Private Sub UpdateRecord(ByRef row As DataRow)
+    Dim UAParser As Parser = Parser.GetDefault()
+    Dim ParsedInfo As ClientInfo = UAParser.Parse(row("UserAgent"))
+    Dim BrowserVersion As String = String.Empty
+    Dim OperatingSystemVersion As String = String.Empty
+    Dim Device As String = GetDevice(ParsedInfo)
+    Dim Browser As String = GetBrowser(ParsedInfo, BrowserVersion)
+    Dim OperatingSystem As String = GetOperatingSystem(ParsedInfo, OperatingSystemVersion)
+    row("Device") = Device
+    row("OperatingSystem") = OperatingSystem
+    row("OperatingSystemVersion") = OperatingSystemVersion
+    row("Browser") = Browser
+    row("BrowserVersion") = BrowserVersion
+    row("LastModified") = Now
+  End Sub
+
+  Private Function GetDevice(ByVal parsedInfo As ClientInfo) As String
+    Dim Device As String = parsedInfo.Device.Family
+    Dim OperatingSystem As String = parsedInfo.OS.Family
+    If Device = "Other" Then
+      If OperatingSystem = "Linux" OrElse OperatingSystem = "Ubuntu" OrElse OperatingSystem.Contains("Windows") OrElse OperatingSystem.Contains("Mac") OrElse OperatingSystem = "Chrome OS" Then
+        Device = "Computer"
+      End If
+    End If
+    Return Device
+  End Function
+
+  Private Function GetBrowser(ByVal parsedInfo As ClientInfo, ByRef browserVersion As String) As String
+    Dim Browser As String = parsedInfo.UserAgent.Family
+    If IsNothing(parsedInfo.UserAgent.Major) = False Then
+      browserVersion = String.Concat(parsedInfo.UserAgent.Major, ".", parsedInfo.UserAgent.Minor)
+    Else
+      browserVersion = String.Empty
+    End If
+    If parsedInfo.ToString.Contains("AOL") Then
+      Browser = "AOL Browser"
+      If parsedInfo.ToString.Contains("AOL/") Then
+        Dim BrowserSplit As String() = parsedInfo.String.Split(New String() {"AOL/"}, StringSplitOptions.RemoveEmptyEntries)
+        Dim BrowserSplit2 As String = BrowserSplit(1)
+        Dim BrowserSplit3 As String() = BrowserSplit2.Split(" ")
+        browserVersion = BrowserSplit3(0)
+      Else
+        browserVersion = String.Empty
+      End If
+    End If
+    Return Browser
+  End Function
+
+  Private Function GetOperatingSystem(ByVal parsedInfo As ClientInfo, ByRef operatingSystemVersion As String) As String
+    Dim OperatingSystem As String = parsedInfo.OS.Family
+    If IsNothing(parsedInfo.OS.Major) = False Then
+      operatingSystemVersion = parsedInfo.OS.Major
+      If IsNothing(parsedInfo.OS.Minor) = False Then
+        operatingSystemVersion &= "." & parsedInfo.OS.Minor
+      End If
+    Else
+      operatingSystemVersion = String.Empty
+    End If
+    If OperatingSystem.Contains("Windows Phone") = False Then
+      Dim Split As String() = OperatingSystem.Split(" ")
+      If Split.Length > 1 Then
+        OperatingSystem = Split(0)
+        operatingSystemVersion = Split(1)
+      End If
+    End If
+    Return OperatingSystem
+  End Function
+
+End Module
